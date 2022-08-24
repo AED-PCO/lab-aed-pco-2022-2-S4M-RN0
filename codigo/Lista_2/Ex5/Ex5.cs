3554197b4370e2ad1e3b8e1646af20d421144901
S4M-RN0 <100299637+S4M-RN0@users.noreply.github.com>
2022-08-24 17:06:48 -0300

    Exercícios 5 e 6 da lista 2

---
+++ codigo/Lista_2/Ex5/Ex5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static int Soma(int[] vet, int vleng)
+        {
+            if (vleng > 0)
+            {
+                if (vet[vleng-1]%2 == 0)
+                {
+                    return vet[vleng-1] + Soma(vet, vleng-1);
+                }
+                else if (vet[vleng-1]%2 != 0)
+                {
+                    return Soma(vet, vleng-1);
+                }
+            }
+
+            return 0;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            int[] vet = new int[5];
+            
+            Console.WriteLine("Digite os valores do vetor");
+
+            for (int i=0; i<vet.Length; i++)
+            {
+                vet[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine("Erro! Digite um número positivo");
+                    i--;
+                }
+            }
+
+            Console.WriteLine($"A soma é {Soma(vet, 5)}");
+        }
+    }
+}
---
+++ codigo/Lista_2/Ex6/Ex6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex6
+{
+    class Program
+    {
+        // Não  finalizada
+           static string Frase(string f, int fleng)
+        { 
+            if (f.Length > 0)
+            {
+                if (f[fleng-1] == 'a' || f[fleng-1] == 'e' || f[fleng-1] == 'i' || f[fleng-1] == 'o' || f[fleng-1] == 'u')
+                {
+                    f = f.Remove(fleng-1, 1);
+                    return Frase(f, f.Length);
+                }
+                else
+                {
+                    return Frase(f, fleng-1);
+                }
+            }
+            
+            return f;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase");
+            String f = Console.ReadLine();
+            f = f.ToLower();
+
+            Console.WriteLine($"A frase modificada é: {Frase(f, f.Length)}");
+        }
+    }
+}
