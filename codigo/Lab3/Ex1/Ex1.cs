989148508c989369453c663b6d4f513970f941eb
S4M-RN0 <100299637+S4M-RN0@users.noreply.github.com>
2022-09-11 01:57:16 -0300

    Exercícios do lab 3

---
+++ codigo/Lab3/Ex1/Ex1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ex1 
+{
+    class Program 
+    {
+
+        //Com pos
+        static int[] vetOrd(int[] vet) 
+        {
+            //int[] vetNovo = new int[];
+            int temp = 0;  
+  
+            for (int i = 0; i < vet.Length; i++)  
+            {  
+                for (int j = i+1; j < vet.Length; j++)  
+                {  
+                    if (vet[i] > vet[j])  
+                    {  
+                        temp = vet[i];  
+                        vet[i] = vet[j];  
+                        vet[j] = temp;  
+                    }  
+                }  
+            }
+
+            return vet;
+        }
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Digite os valores do vetor:");
+            int[] vet = new int[10];
+            
+            for (int i=0; i < vet.Length; i++)
+            {
+                vet[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] vetNovo = vetOrd(vet);
+
+            Console.WriteLine("\nVetor ordenado:");
+            for (int i=0; i < vet.Length; i++) 
+            {
+                Console.WriteLine(vet[i]);
+            }
+        }
+    }
+}
---
+++ codigo/Lab3/Ex2/Ex2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static string Busca(int[] vet, int num)
+        {
+            int min = 0;
+            int max = vet.Length - 1;
+            int mid = (min + max) / 2;
+
+            while (min <=max) 
+            {
+                mid = (min + max) / 2;
+
+                if (num > vet[mid]) 
+                {
+                    min = mid + 1;
+                    
+                } 
+                else if (num < vet[mid]) 
+                {
+                    max = mid - 1;
+                }
+                else return mid.ToString();
+            }
+
+            return "Número não encontrado";
+        }
+
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10] {-20, -6, 2, 7, 10, 13, 23, 28, 45, 65};
+
+            Console.WriteLine("Digite o número que você quer encontrar:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(Busca(vet, num));
+        }
+    }
+}
